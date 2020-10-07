3a45577ea16e92eef4a7202a424e517cfae9de79
Girlc7442 <girlc7442@wrdsb.ca>
2020-10-07 16:19:10 +0000

    Working on the isosceles triangle

--- Main.java
+++ Main.java
@@ -21,27 +21,14 @@ public class Main {
     //loop runs until i equals the user number
     for(int i = 1; i <= userNumber; i++){
       
-      //loop runs until j equals the user number
-      
-      for(int j = 1; j <= userNumber; j++){
-       //if i is equal to 1 or the user number will print asterisks to the screen equal to the user number
-       if(i == 1 || i == userNumber){
+      //follows i and prints asterisks equal to the number  that i is on
+      for(int j = 1; j <= i; j++){
         System.out.print("*");
-        }
-       //if i is greater than or equal to 2 and i is less than the user number will run the if statement inside
-       if(i >= 2 && i < userNumber){
-        //if j is equal to 1 or the user number will print an asterisk at the beginnig and end of a line
-        if(j == 1 || j == userNumber){
-        System.out.print("*");
-         }
-        }
-        //if j is greater than or equal to 2 and j is less than the user number and i is greater than or equal to 2 and i is less than the user number then this will print a spacein the middle of a line
-      if(j >= 2 && j < userNumber && i >= 2 && i < userNumber){
-        System.out.print(" ");
-          
-        }
        }
       
+      if(i == userNumber){
+        i-=2;
+      }
       // prints a blank space to space the asteriks out
       System.out.println();
        
