7ca22de5ba05b5de22a3af26b5d3a46ba6648292
Girlc7442 <girlc7442@wrdsb.ca>
2020-10-07 14:22:47 +0000

    Finished with comments

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /**
- * Prints a line of asterisks to the screen based on the number the user enters then repeats that line based on the number the user enters
+ * Prints a box of asterisks to the screen based on the number the user enters 
  * @author Cole Girling
  */
 public class Main {
@@ -21,31 +21,30 @@ public class Main {
     //loop runs until i equals the user number
     for(int i = 1; i <= userNumber; i++){
       
-      //runs until j equals the user number
+      //loop runs until j equals the user number
       
-       for(int j = 1; j <= userNumber; j++){
-        //prints an asterisk to the screen
-        if(i == 1 || i == userNumber){
+      for(int j = 1; j <= userNumber; j++){
+       //if i is equal to 1 or the user number will print asterisks to the screen equal to the user number
+       if(i == 1 || i == userNumber){
         System.out.print("*");
         }
-        if(i >= 2 && i <= userNumber){
-          if(j == 1 || j == userNumber){
-          System.out.print("*");
+       //if i is greater than or equal to 2 and i is less than the user number will run the if statement inside
+       if(i >= 2 && i < userNumber){
+        //if j is equal to 1 or the user number will print an asterisk at the beginnig and end of a line
+        if(j == 1 || j == userNumber){
+        System.out.print("*");
          }
         }
-        if(j >= 2 && j < userNumber && i >= 2 && i < userNumber){
-          System.out.print(" ");
+        //if j is greater than or equal to 2 and j is less than the user number and i is greater than or equal to 2 and i is less than the user number then this will print a spacein the middle of a line
+      if(j >= 2 && j < userNumber && i >= 2 && i < userNumber){
+        System.out.print(" ");
           
         }
        }
       
-      
-        
-        
-        // prints a blank space to space the asteriks out
-        System.out.println();
-        
-        
+      // prints a blank space to space the asteriks out
+      System.out.println();
+       
     }
     
   }
